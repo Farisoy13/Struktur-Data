0a6faa05a525675333a25bfc6d7b0301d9f4f000
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 23:13:14 +0700

    Update no2.cpp

--- Pertemuan 6 Linear Queue/Latihan Soal.cpp/no2.cpp
+++ Pertemuan 6 Linear Queue/Latihan Soal.cpp/no2.cpp
@@ -1,1 +1,15 @@
+Deklarasi:
+    Queue Q
+    Record data
+    integer count ← 0
+
+Deskripsi:
+    Selama (count < 10) dan (antrian tidak kosong) lakukan:
+        hapus data dari antrian (DEQUEUE(Q, data))
+        count ← count + 1
+    Jika antrian kosong:
+        tampilkan "Antrian kosong, proses delete dihentikan"
+    Jika count = 10:
+        tampilkan "Sudah menghapus 10 record"
+Selesai
 
