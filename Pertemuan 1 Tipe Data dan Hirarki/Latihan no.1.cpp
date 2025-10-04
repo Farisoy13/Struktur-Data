b8bb43b1c2f4c47f183d41f1a4dfcd0a75e348b0
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 18:25:43 +0700

    Add files via upload

---
+++ Pertemuan 1 Tipe Data dan Hirarki/Latihan no.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka;
+    char huruf;
+    float pecahan;
+
+    // input 3 nilai dipisah spasi
+    cin >> angka >> huruf >> pecahan;
+
+    cout << angka << endl;
+    cout << huruf << endl;
+    cout << pecahan << endl;
+
+    return 0;
+}
+
