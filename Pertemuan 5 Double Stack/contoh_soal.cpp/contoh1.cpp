096bc88345b685460ea0ed18f4926f4ea50b38ab
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 23:05:10 +0700

    Update contoh1.cpp

--- Pertemuan 5 Double Stack/contoh_soal.cpp/contoh1.cpp
+++ Pertemuan 5 Double Stack/contoh_soal.cpp/contoh1.cpp
@@ -1,1 +1,15 @@
-
+#define MAX 5
+//Deklarasi Double Stack
+class DStack
+{
+private:
+int top1;
+int top2;
+int ele[MAX];
+public:
+DStack();
+void pushA(int item);
+void pushB(int item);
+int popA (int *item);
+int popB (int *item);
+};
