b128a0e2ab54a1d5512712cbc7b634ffc761635a
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 23:29:03 +0700

    Update no3.cpp

--- Pertemuan 3 Struktur atau Record/Latihan Soal.cpp/no3.cpp
+++ Pertemuan 3 Struktur atau Record/Latihan Soal.cpp/no3.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    int noUrut;
+    string nama;
+    int umur;
+    string alamat;
+    float nilai;
+};
+
+int main() {
+    Mahasiswa mhs[15];
+
+    cout << "Masukkan Data 15 Mahasiswa:\n";
+    for (int i = 0; i < 15; i++) {
+        mhs[i].noUrut = i + 1;
+        cout << "\nMahasiswa ke-" << mhs[i].noUrut << endl;
+        cin.ignore();
+        cout << "Nama    : ";
+        getline(cin, mhs[i].nama);
+        cout << "Umur    : ";
+        cin >> mhs[i].umur;
+        cin.ignore();
+        cout << "Alamat  : ";
+        getline(cin, mhs[i].alamat);
+        cout << "Nilai   : ";
+        cin >> mhs[i].nilai;
+    }
+
+    cout << "\n=== Data Seluruh Mahasiswa ===\n";
+    for (int i = 0; i < 15; i++) {
+        cout << "\nNo Urut : " << mhs[i].noUrut;
+        cout << "\nNama    : " << mhs[i].nama;
+        cout << "\nUmur    : " << mhs[i].umur;
+        cout << "\nAlamat  : " << mhs[i].alamat;
+        cout << "\nNilai   : " << mhs[i].nilai << endl;
+    }
+
+    return 0;
+}
 
