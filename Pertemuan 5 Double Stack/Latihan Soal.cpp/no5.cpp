7871acb39057f79ca4788c9bb7f74c114324bfbe
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 23:21:47 +0700

    Update no5.cpp

--- Pertemuan 5 Double Stack/Latihan Soal.cpp/no5.cpp
+++ Pertemuan 5 Double Stack/Latihan Soal.cpp/no5.cpp
@@ -1,1 +1,11 @@
+Algoritma CETAK_STACK2
+Deskripsi:
+1. Jika TOP2 == N + 1 maka
+       Cetak "Stack2 kosong!"
+   else
+       while TOP2 <= N do
+           Cetak STACK[TOP2]
+           TOP2 ← TOP2 + 1
+       endwhile
+   endif
 
