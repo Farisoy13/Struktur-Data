16bfada21cbb899b775752e5761b4f7589ebf5dc
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 23:26:05 +0700

    Update no3.cpp

--- Pertemuan 4 Single Stack/Latihan Soal_cpp/no3.cpp
+++ Pertemuan 4 Single Stack/Latihan Soal_cpp/no3.cpp
@@ -1,1 +1,10 @@
+// Potongan program PUSH
+if (top < 9) {
+    top++;
+    cout << "Masukkan data: ";
+    cin >> S[top];
+    cout << "Data " << S[top] << " berhasil di-PUSH.\n";
+} else {
+    cout << "Stack Penuh!\n";
+}
 
