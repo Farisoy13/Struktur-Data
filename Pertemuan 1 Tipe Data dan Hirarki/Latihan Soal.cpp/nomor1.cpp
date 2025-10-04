a1cc865e1108704ff0987d3726b6cc23a04c2a51
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-04 18:29:56 +0700

    Update nomor1.cpp

--- Pertemuan 1 Tipe Data dan Hirarki/Latihan Soal.cpp/nomor1.cpp
+++ Pertemuan 1 Tipe Data dan Hirarki/Latihan Soal.cpp/nomor1.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka;
+    char huruf;
+    float pecahan;
+
+    // input 3 nilai dipisah spasi
+    cin >> angka >> huruf >> pecahan;
+
+    cout << angka << endl;
+    cout << huruf << endl;
+    cout << pecahan << endl;
+
+    return 0;
+}
+
 
 
 
