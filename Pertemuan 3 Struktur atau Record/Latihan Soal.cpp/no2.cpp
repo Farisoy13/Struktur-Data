32b4d2d8f516cb98225819273c232b78da13448a
FARISA NADHIRA EL-FANSYAH <farisanadhira13@gmail.com>
2025-10-11 17:09:18 +0700

    Update no2.cpp

--- Pertemuan 3 Struktur atau Record/Latihan Soal.cpp/no2.cpp
+++ Pertemuan 3 Struktur atau Record/Latihan Soal.cpp/no2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct Mahasiswa {
-    int noUrut;
+    int no_urut;
     string nama;
     int umur;
 };
@@ -10,22 +10,21 @@ struct Mahasiswa {
 int main() {
     Mahasiswa mhs[5];
 
-    cout << "Masukkan Data 5 Mahasiswa:\n";
     for (int i = 0; i < 5; i++) {
-        mhs[i].noUrut = i + 1;
-        cout << "\nMahasiswa ke-" << mhs[i].noUrut << endl;
-        cout << "Nama  : ";
-        cin.ignore();
-        getline(cin, mhs[i].nama);
-        cout << "Umur  : ";
+        mhs[i].no_urut = i + 1;
+        cout << "Masukkan Nama Mahasiswa " << i + 1 << ": ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan Umur: ";
         cin >> mhs[i].umur;
+        cout << endl;
     }
 
-    cout << "\n=== Detail Mahasiswa dengan No Urut 2 ===\n";
-    cout << "No Urut : " << mhs[1].noUrut << endl;
-    cout << "Nama    : " << mhs[1].nama << endl;
-    cout << "Umur    : " << mhs[1].umur << endl;
+    cout << "\n=== Data Mahasiswa No Urut 2 ===\n";
+    cout << "No Urut: " << mhs[1].no_urut << endl;
+    cout << "Nama   : " << mhs[1].nama << endl;
+    cout << "Umur   : " << mhs[1].umur << endl;
 
     return 0;
 }
+
 
